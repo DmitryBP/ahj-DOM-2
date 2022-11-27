a750dc797c16bad0ae9e4b659e47fe81be19d663
Dmitry_home <79811924115@yandex.ru>
2022-11-27 13:12:39 +0300

    test

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,40 @@
-import sum from './sum';
+/* eslint-disable eqeqeq */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-console */
+/* global document */
+const button = document.querySelectorAll('button.button');
+const item = document.querySelectorAll('div.item');
 
-console.log('it works!!');
+const arr = [];
+for (let j = 0; j <= 150; j++) {
+  const g = Math.floor(Math.random() * 16);
+  arr.push(g);
+}
+const unicArr = [...new Set(arr)];
 
-console.log(sum(2, 2));
+let i = 0;
+button[0].onclick = () => {
+  setInterval(() => {
+    if (i < unicArr.length - 1) {
+      if (i == 0) {
+        item[i].classList.add('test');
+      } else {
+        item[0].classList.remove('test');
+        item[unicArr[i]].classList.add('test');
+        if (i != 0) {
+          item[unicArr[i - 1]].classList.remove('test');
+        } else {
+          item[unicArr[i]].classList.remove('test');
+        }
+      }
+      i++;
+    } else console.log('The end');
+  }, 1000);
+};
+const score = 0;
+console.log(score);
+document.addEventListener('click', (e) => {
+  const { target } = e;
+  console.log(target.className.includes('test'));
+});
